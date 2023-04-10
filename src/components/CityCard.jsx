8bc6c72930820abc25800fd50362d2d179eb6b63
Iván <ivantbd87@gmail.com>
2023-04-10 23:55:47 +0200

    sizes test

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -7,8 +7,8 @@ function CityCard({ name, stateSky, temperatures, idProvince, id }) {
     <Link to={`/provincias/${idProvince}/municipios/${id}`} className="text-decoration-none" >
       <div className=' weather-card'>
         <div className='card-text border d-flex flex-column align-items-center justify-content-center bg-secondary p-2'>
-          <h4 className='text-center patata d-inline-block text-truncate' style={{maxWidth: "150px"}}>{name}</h4> 
-          <h5 className='text-center patata d-inline-block text-truncate' style={{maxWidth: "150px"}}>{stateSky.description}</h5> 
+          <h4 className='text-center patata d-inline-block text-truncate' style={{maxWidth: "120px"}}>{name}</h4> 
+          <h5 className='text-center patata d-inline-block text-truncate' style={{maxWidth: "120px"}}>{stateSky.description}</h5> 
           <div className='row'>
             <h5 className='col-5 text-center'>Min: {temperatures.min}</h5>
             <h5 className='col-5 text-center'>Max: {temperatures.max}</h5>
